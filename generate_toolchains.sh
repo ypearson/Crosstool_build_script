78d28f6e5f9095a1a59452aeb59f2fc955ce6426
Yvan Pearson <ypearson@nklabs.com>
2015-07-03 22:45:53 -0400

    multiple toolchain builds script update.

--- generate_toolchains.sh
+++ generate_toolchains.sh
@@ -4,29 +4,34 @@
 # USER HOSTNAME = (root) NOPASSWD: /usr/bin/apt-get
 # USER HOSTNAME = (root) NOPASSWD: /usr/bin/make
 
-TC=arm-unknown-linux-gnueabi
+TC="arm-unknown-linux-gnueabi \
+    arm-unknown-linux-uclibcgnueabi 
+    arm-unknown-linux-uclibcgnueabihf"
+
 DEST=.
 CTVER=1.20.0 # v1.21 doesn't seem build correctly on Ubuntu 14.04
 URL=http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-${CTVER}.tar.bz2
-
-rm -rf ${HOME}/x-tools
 
 wget -qO- ${URL} | tar -C ${DEST} -xvjf -
 
 sudo apt-get install -y gperf bison flex texinfo gawk libtool automake libncurses5-dev g++ libexpat1-dev
 
+rm -rf ${HOME}/x-tools
 cd crosstool-ng-${CTVER}
 ./configure
 make
 sudo make install
 
-ct-ng ${TC}
-ct-ng build
+
+for tc in $TC
+do
+	ct-ng $tc
+	ct-ng build
+done
 
 cd ${HOME}/x-tools
-tar -czf ${TC}.tar.gz ${TC}
 
-
-
-
-
+for tc in $TC
+do
+	tar -czf ${tc}.tar.gz ${tc}
+done
