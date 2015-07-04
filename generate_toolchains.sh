c430167b67c8c2e7039a660061e81c8b62b20688
Yvan Pearson <ypearson@nklabs.com>
2015-07-04 08:35:05 -0400

    tweek of script to clean up work space.

--- generate_toolchains.sh
+++ generate_toolchains.sh
@@ -9,19 +9,29 @@ TC="arm-unknown-linux-gnueabi \
     arm-unknown-linux-uclibcgnueabihf"
 
 DEST=.
-CTVER=1.20.0 # v1.21 doesn't seem build correctly on Ubuntu 14.04
+CWD=$PWD
+CTVER=1.20.0 # v1.21 doesn't seem to build correctly on Ubuntu 14.04
 URL=http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-${CTVER}.tar.bz2
+XDIR=${HOME}/x-tools
+
+if [ -d crosstool-ng-${CTVER} ]; then
+   echo 'Deleting old crosstool-ng.'
+   rm -rf crosstool-ng-${CTVER}
+fi
+
+if [ -d ${XDIR} ]; then
+	echo 'Deleting old toolchain build.'
+	chmod 777 -R ${XDIR}
+	rm -rf ${XDIR}
+fi
 
 wget -qO- ${URL} | tar -C ${DEST} -xvjf -
-
 sudo apt-get install -y gperf bison flex texinfo gawk libtool automake libncurses5-dev g++ libexpat1-dev
 
-rm -rf ${HOME}/x-tools
 cd crosstool-ng-${CTVER}
 ./configure
 make
 sudo make install
-
 
 for tc in $TC
 do
@@ -29,9 +39,14 @@ do
 	ct-ng build
 done
 
-cd ${HOME}/x-tools
+cd ${XDIR}
 
 for tc in $TC
 do
 	tar -czf ${tc}.tar.gz ${tc}
+	mv ${tc}.tar.gz ${CWD}
 done
+
+cd ${CWD}
+chmod 777 -R ${XDIR}
+rm -rf ${XDIR}
